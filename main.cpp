efbe89bf50efdcb1b9023eaa28a425c679b53023
djabby <djabby@gmail.com>
2011-09-03 19:53:52 +0200

    works now._.

--- main.cpp
+++ main.cpp
@@ -13,11 +13,6 @@ int main()
     gui::Widget window(sf::FloatRect(200,0,App.GetWidth(),App.GetHeight()));
     gui::Label label(sf::FloatRect(-150,0,200,40),sf::String("Hallo"));
     window.addChild(label);
-    // Load a sprite to display
-    sf::Image Image;
-    if (!Image.LoadFromFile("cb.bmp"))
-        return EXIT_FAILURE;
-    sf::Sprite Sprite(Image);
     int i = 0;
 	// Start the game loop
     while (App.IsOpened())
@@ -32,7 +27,6 @@ int main()
         }
 
         App.Clear();
-        //App.Draw(Sprite);
         App.SetView(App.GetDefaultView());
         label.string.SetText(text::add((std::string)sf::Unicode::Text("Nummer: "),i++));
         window.draw(App);
